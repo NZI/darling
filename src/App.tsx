b2949cb274ae27e721a9623734b206fa1c9179d0
Isaac Gilmour <isaac.gilmour@we-are-mea.com>
2023-02-28 14:29:57 +1300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import './App.scss';
+import { AiOutlinePlayCircle } from 'react-icons/ai'
+
+
+const songs: any[] = [
+  new Audio('/songs/falling in love with you.mp3'),
+  new Audio('/songs/i walk the line.mp3'),
+  new Audio('/songs/killing me softly.mp3'),
+  new Audio('/songs/my sunshine.mp3'),
+  new Audio('/songs/the only exception.mp3'),
+]
 
 function App() {
+  const [fadeClassList, setFadeClassList] = useState('play fade')
+  const [rotate, setRotate] = useState(0)
+  const [color, setColor] = useState(173)
+  const [songIndex, setSongIndex] = useState(-1)
+
+  const playClicked = () => {
+
+    let newSongIndex = Math.floor(Math.random() * songs.length)
+
+    while (newSongIndex === songIndex) {
+      newSongIndex = Math.floor(Math.random() * songs.length)
+    }
+
+    songs.forEach((s, i) => {
+      if (i !== newSongIndex) {
+        if (!s) {
+          return
+        }
+        s.pause()
+        s.currentTime = 0
+      }
+    })
+
+    setFadeClassList('play fade fade-start')
+    setColor(Math.floor(Math.random() * 36000) / 100)
+    setSongIndex(newSongIndex)
+
+    songs[newSongIndex]?.play?.()
+
+    setTimeout(() => {
+      setFadeClassList('play fade fade-start fading')
+      setRotate(rotate + 1)
+    }, 200)
+    setTimeout(() => {
+      setFadeClassList('play fade')
+    }, 700)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ color: `hsl(${color}, 50%, 70%)` }}>
+      <div className="rotate" style={{
+        transform: `rotate(${rotate * 360}deg)`
+      }}>
+        <AiOutlinePlayCircle className='play' onClick={playClicked} />
+        <AiOutlinePlayCircle className={fadeClassList} />
+      </div>
     </div>
   );
 }
